3b1a2b40020c408fd4c952d21a4bd15158eaad9e
000Diggity000 <116099377+000Diggity000@users.noreply.github.com>
2024-03-06 00:04:36 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace NoAbiltySpawn
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, "No Abilty Spawn", PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = new Harmony("com.Diggity.NoAbilitySpawn");
+            MethodBase original = AccessTools.Method(typeof(AbilitySpawner), "Awake");
+            harmony.Patch(original, new HarmonyMethod(AccessTools.Method(typeof(myPatches), "NoAbilitySpawn")));
+        }
+    }
+    public class myPatches
+    {
+        public static bool NoAbilitySpawn(AbilitySpawner __instance)
+        {
+            __instance.enabled = false;
+            return false;
+        }
+
+    }
+}
